8098fc93f5b665e97365f7b38c1098d327600bb7
Christian Mesa <christian.mesa98.cm@gmail.com>
2024-11-06 17:09:51 -0500

    Conn string endpoint

--- CourseCreator/obj/Debug/net8.0/CourseCreator.AssemblyInfo.cs
+++ CourseCreator/obj/Debug/net8.0/CourseCreator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CourseCreator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd0b8a96104cd2909a10a840d0b52d2a1c35b174")]
 [assembly: System.Reflection.AssemblyProductAttribute("CourseCreator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CourseCreator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
